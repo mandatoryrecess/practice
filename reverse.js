bab8d7882fa7d0851c221442916720be6ecc812e
rossanne <rossanneclamp@gmail.com>
2022-02-23 20:25:41 -0800

    without built in functions

--- reverse.js
+++ reverse.js
@@ -3,3 +3,12 @@ let string = "hello";
 const newString = string.split('').reverse().join(''); 
 
 console.log(newString)
+
+//without built in functions 
+
+let text = "Ww"; 
+let newText = '';
+for (let x = text.length - 1; x >= 0; x--) {
+  newText += text[x]; 
+}
+console.log(newText)
