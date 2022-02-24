2b0dc1843e73bcb93767263d547e461b9d9ac236
rossanne <rossanneclamp@gmail.com>
2022-02-23 20:31:42 -0800

    new string

--- reverse.js
+++ reverse.js
@@ -6,9 +6,10 @@ console.log(newString)
 
 //without built in functions 
 
-let text = "Ww"; 
-let newText = '';
+let text = "Let's See If I Can Reverse You"; 
+let newText = " "; 
+
 for (let x = text.length - 1; x >= 0; x--) {
-  newText += text[x]; 
+  newText += text[x]
 }
-console.log(newText)
+console.log(newText);
